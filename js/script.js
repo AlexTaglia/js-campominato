3c083ac2bcabf040043135902134a1100b056ead
AlexTaglia <alex.tagliabue@gmail.com>
2021-06-10 15:49:03 +0200

    Preparazione seconda parte

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ con difficoltà 2 => tra 1 e 50
 
 */
 
+// --------------------------------------------------------------------------
 // 1 creo un'array che conterrà i miei 16 numeri
 // 2 creo una funzione che mi generi 16 numeri casuali
 // 2.1 Verifico con while se il numero random generato non è gia nell'array e se non è presente lo pusho
@@ -26,9 +27,9 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function isNotInArray(array, element) {
-    for (var x = 0; x < array.length; x++) {
-        if (element !== array[x]) {
+function isInArray(array, element) {
+    for (var i = 0; i < array.length; i++) {
+        if (element === array[i]) {
             return true;
         }
     }
@@ -37,13 +38,21 @@ function isNotInArray(array, element) {
 
 var listNumPC = [];
 var randomNumsQty = 16;
+var maxBomb = 100;
 
 while (listNumPC.length < randomNumsQty) {
-    var randomNum = getRandomNumber(1, 100)
+    var randomNum = getRandomNumber(1, maxBomb)
 
-    if (isNotInArray(listNumPC, randomNum)){
+    if (!isInArray(listNumPC, randomNum)){
         listNumPC.push(randomNum);
     }
 }
 
 console.log(listNumPC);
+// creata lista 16 numeri non duplicati
+
+// --------------------------------------------------------------------------
+// chiedo all'utente per (maxBomb - randomNumsQty.lenght) di inserire un numero alla volta tra 1 e maxBomb
+// !!!l'utente non puo inserire un numero giainserto, 
+//  se numUtente isInArray listNumPC partita termina else pusho il num in numUtente e cosi via
+//  stmpo il risultato numUtente.lenght a ogni ciclo
